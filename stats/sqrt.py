3a70b12a249cb53ffc3871b6933950156179cef7
parrt <parrt@cs.usfca.edu>
2015-07-07 14:58:12 -0700

    update pointadd more files

---
+++ stats/sqrt.py
@@ -0,0 +1,8 @@
+# Stop iterating when the new approximation is within
+# PRECISION of the old value.
+PRECISION = 0.000000001
+
+# compute square root of n
+def sqrt(n):
+    ...
+
---
+++ stats/stats.py
@@ -0,0 +1,9 @@
+# sum of values / num elements
+def mean(x):
+
+# sum of diff from mean squared (unbiased)
+def var(x):
+
+# sum of diff from mean squared (unbiased)
+def cov(x,y):
+
