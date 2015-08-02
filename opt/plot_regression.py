8ba13a59ed2c114747508b472070de01e77885a0
Terence Parr <parrt@antlr.org>
2015-08-02 13:37:15 -0700

    move methods around in regression kit

--- opt/plot_regression.py
+++ opt/plot_regression.py
@@ -7,11 +7,9 @@ HOURLY_WAGE = [2.98, 3.09, 3.23, 3.33, 3.46, 3.6, 3.73, 2.91, 4.25, 4.47, 5.04, 
 # Number of homicides per 100,000 people
 MURDERS = [8.6, 8.9, 8.52, 8.89, 13.07, 14.57, 21.36, 28.03, 31.49, 37.39, 46.26, 47.24, 52.33]
 
-def Cost(B,X=HOURLY_WAGE,Y=MURDERS):
-	cost = 0.0
-	for i in xrange(0,len(X)):
-		...
-	return cost
+def heatmap(X, Y, f, trace=None): # trace is a list of [b1, b2] pairs
+	...
+
 
 random.seed(int(round(time.time() * 1000)))
 
--- opt/regression.py
+++ opt/regression.py
@@ -2,10 +2,14 @@ import numpy as np
 from pylab import imshow, plot
 import matplotlib.pyplot as plt
 
+def Cost(B,X=HOURLY_WAGE,Y=MURDERS):
+	cost = 0.0
+	for i in xrange(0,len(X)):
+		...
+	return cost
+
 def minimize(f, B0, eta, h, precision):
 	trace = []
 	...
 	return (B,steps,trace)
 
-def heatmap(X, Y, f, trace=None): # trace is a list of [b1, b2] pairs
-	...
