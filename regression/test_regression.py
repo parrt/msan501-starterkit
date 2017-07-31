7389c231dd5cb8edd2e2790d99d14d6da4a3c2b2
parrt <parrt@cs.usfca.edu>
2017-07-31 14:59:27 -0700

    tweak

--- regression/test_regression.py
+++ regression/test_regression.py
@@ -7,8 +7,8 @@ HOURLY_WAGE = [2.98, 3.09, 3.23, 3.33, 3.46, 3.6, 3.73, 2.91, 4.25, 4.47, 5.04, 
 # Number of homicides per 100,000 people
 MURDERS     = [8.6, 8.9, 8.52, 8.89, 13.07, 14.57, 21.36, 28.03, 31.49, 37.39, 46.26, 47.24, 52.33]
 
-LEARNING_RATE = ...
-h = ...
+LEARNING_RATE = ... # fill in with your values
+h = ... # fill in with your values
 PRECISION = 0.000001
 
 def Cost(B,X=HOURLY_WAGE,Y=MURDERS):
